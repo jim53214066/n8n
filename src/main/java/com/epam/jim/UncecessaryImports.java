e4beff3abdba95b04f6535c0a6b01f838e5dd9fd
jim53214066 <48121013+jim53214066@users.noreply.github.com>
2024-05-28 14:33:44 +0800

    update by n8n

--- src/main/java/com/epam/jim/UncecessaryImports.java
+++ src/main/java/com/epam/jim/UncecessaryImports.java
@@ -1,14 +1,8 @@
 package com.epam.jim;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.io.File;
 
-public class UncecessaryImports {
-
+public class UnnecessaryImports {
 
     public void processFile(File file) {
         System.out.println("input file: " + ((file == null) ? "Null" : file.getName()));
